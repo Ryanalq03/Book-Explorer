96d5cdb715d9a8433a7af3d2cc979730c9c4c86d
Ryan Alqasemi <alqasemir@usf.edu>
2025-04-12 15:04:48 -0400

    App setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import BookList from './components/BookList';
 
+//Root component of the app
 function App() {
-  const [count, setCount] = useState(0)
+//global state to hold list of books
+const [books, setBooks] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+// Function to remove a book by its ID
+const removeBook = (id) => {
+    setBooks(books.filter((book) => book.id !== id));
+};
+return (
+    <main>
+        <h1>Book Explorer</h1>
+        <BookList books={books} setBooks={setBooks} onRemove={removeBook} />
+
+    </main>
+)
 }
 
-export default App
+export default App;
